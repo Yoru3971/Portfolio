2272cb3cc476fa07a8e90df97f9045fbf2ed2ff3
Emiliano Daverio <150267786+Yoru3971@users.noreply.github.com>
2026-02-20 10:38:51 -0300

    added i18n configuration and json files for language control

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,41 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+
+export class CustomTranslateLoader implements TranslateLoader {
+  constructor(private http: HttpClient) {}
+
+  getTranslation(lang: string): Observable<any> {
+    return this.http.get(`assets/i18n/${lang}.json`);
+  }
+}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new CustomTranslateLoader(http);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { NavBar } from './shared/components/nav-bar/nav-bar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,15 @@ import { NavBar } from './shared/components/nav-bar/nav-bar';
 })
 export class App {
   protected readonly title = signal('portfolio');
+
+  constructor(private translate: TranslateService) {
+    // Adds the languages you will support
+    this.translate.addLangs(['es', 'en']);
+    
+    // Sets the fallback language
+    this.translate.setDefaultLang('es');
+
+    // Sets the active language
+    this.translate.use('es');
+  }
 }
