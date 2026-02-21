c68b46e6f328a1ae01170ffd623d72492eda42df
Emiliano Daverio <emilianodaverio@gmail.com>
2026-02-20 21:35:29 -0300

    added contact component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,12 @@ import { LanguageService } from './core/services/language-service';
 import { AboutMeComponent } from './shared/components/about-me-component/about-me-component';
 import { SkillsComponent } from './shared/components/skills-component/skills-component';
 import { ProjectsComponent } from './shared/components/projects-component/projects-component';
+import { ContactComponent } from './shared/components/contact-component/contact-component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [NavBar, HeroComponent, AboutMeComponent, SkillsComponent, ProjectsComponent],
+  imports: [NavBar, HeroComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
